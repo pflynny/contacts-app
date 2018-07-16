c8a36ee81fdde49a285c0410ea4a528e4eb76435
Paul Flynn <paul@kernl.co.nz>
2018-07-17 10:57:01 +1200

    Added prop types

--- src/App.js
+++ src/App.js
@@ -1,39 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ListContacts from './ListContacts'
 
-const contacts = [{
-    "id": "karen",
-    "name": "Karen Isgrigg",
-    "handle": "karen_isgrigg",
-    "avatarURL": "http://localhost:5001/karen.jpg"
-  },
-  {
-    "id": "richard",
-    "name": "Richard Kalehoff",
-    "handle": "richardkalehoff",
-    "avatarURL": "http://localhost:5001/richard.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "handle": "tylermcginnis",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
+
+
+
+class App extends Component {
+  state = {
+    contacts: [
+      {
+          "id": "karen",
+          "name": "Karen Isgrigg",
+          "handle": "karen_isgrigg",
+          "avatarURL": "http://localhost:5001/karen.jpg"
+        },
+        {
+          "id": "richard",
+          "name": "Richard Kalehoff",
+          "handle": "richardkalehoff",
+          "avatarURL": "http://localhost:5001/richard.jpg"
+        },
+        {
+          "id": "tyler",
+          "name": "Tyler McGinnis",
+          "handle": "tylermcginnis",
+          "avatarURL": "http://localhost:5001/tyler.jpg"
+        }
+    ]
   }
-];
 
+  removeContact = (contact) => {
+    this.setState((currentState) => ({
+      contacts: currentState.contacts.filter((c) => {
+        return c.id !== contact.id
+      })
+    }))
+  }
 
-const App = () => {
-
-  return (
-    <div>
-      <ListContacts contacts={contacts}/>
-    </div>
-  )
-
+  render() {
+    return (
+      <div>
+        <ListContacts
+          contacts={this.state.contacts}
+          onDeleteContact={this.removeContact}
+        />
+      </div>
+    )
+  }
 }
 
-// class App extends Component {
-
-// }
 
 export default App;
--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const ListContacts = props => {
     return (
@@ -15,8 +16,11 @@ const ListContacts = props => {
                         <p>{contact.name}</p>
                         <p>{contact.handle}</p>
                     </div>
-                    <button className="contact-remove">
-                        Remove
+                    <button
+                        className="contact-remove"
+                        onClick={() => props.onDeleteContact(contact)}
+                    >
+                            Remove
                     </button>
                 </li>
             ))}
@@ -24,40 +28,12 @@ const ListContacts = props => {
     )
 }
 
+ListContacts.propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired,
 
+}
 
-
-
-
-
-
-
-// class ListContacts extends Component {
-//     render() {
-//         console.log("tets" + this.props)
-//         return (
-//             <ol className="contact-list">
-//                 {this.props.contacts.map((contact) => (
-//                   <li className="contact-list-item" key={contact.id}>
-//                     <div
-//                         className="contact-avatar"
-//                         style={{
-//                             backgroundImage: `url(${contact.avatarURL})`
-//                         }}
-//                     ></div>
-//                     <div className="contact-details">
-//                         <p>{contact.name}</p>
-//                         <p>{contact.handle}</p>
-//                     </div>
-//                     <button className="contact-remove">
-//                         Remove
-//                     </button>
-//                   </li>
-//                 ))}
-//             </ol>
-//         )
-//     }
-// }
 
 export default ListContacts
 
