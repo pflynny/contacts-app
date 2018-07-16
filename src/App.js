41c23f80193c369b01e4cfa651c0e24e4759a61f
Paul Flynn <paul@kernl.co.nz>
2018-07-16 14:09:54 +1200

    Created a component for contacts list, turned code into stateless functional components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React, { Component } from 'react';
+import React from 'react'
+import ListContacts from './ListContacts'
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello World
-      </div>
-    )
+const contacts = [{
+    "id": "karen",
+    "name": "Karen Isgrigg",
+    "handle": "karen_isgrigg",
+    "avatarURL": "http://localhost:5001/karen.jpg"
+  },
+  {
+    "id": "richard",
+    "name": "Richard Kalehoff",
+    "handle": "richardkalehoff",
+    "avatarURL": "http://localhost:5001/richard.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "handle": "tylermcginnis",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
   }
+];
+
+
+const App = () => {
+
+  return (
+    <div>
+      <ListContacts contacts={contacts}/>
+    </div>
+  )
+
 }
+
+// class App extends Component {
+
+// }
 
 export default App;
